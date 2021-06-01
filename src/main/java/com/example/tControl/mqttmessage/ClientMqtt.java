6f54ee212ca38e9994b5bc860f545c2fc2897d68
Андрей <@10.0.1.120>
2021-06-01 17:38:34 +0500

    qwe

--- src/main/java/com/example/tControl/mqttmessage/ClientMqtt.java
+++ src/main/java/com/example/tControl/mqttmessage/ClientMqtt.java
@@ -1,32 +1,15 @@
 package com.example.tControl.mqttmessage;
 
-import java.io.ByteArrayInputStream;
-import java.sql.SQLException;
 import java.time.LocalDateTime;
-import java.util.List;
 import java.util.UUID;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-import javax.persistence.Query;
-
 import org.eclipse.paho.client.mqttv3.*;
+import org.eclipse.paho.client.mqttv3.persist.MqttDefaultFilePersistence;
 
 import com.example.tControl.base.PastEmployeesBase;
 import com.example.tControl.component.MessageTemperatureComponent;
-import com.example.tControl.component.PushVerticalLayout;
 import com.example.tControl.pojo.Employee;
 import com.example.tControl.views.Desktop;
-import com.vaadin.flow.component.Component;
-import com.vaadin.flow.component.html.H5;
-import com.vaadin.flow.component.html.H6;
-import com.vaadin.flow.component.html.Image;
-import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
-import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.server.StreamResource;
-
-import java.util.UUID;
 
 public class ClientMqtt {
 	
@@ -37,7 +20,7 @@ public class ClientMqtt {
 	public static void setClientMqtt(/*final List<Component> itemsList, PushVerticalLayout push*/) throws MqttException {
 		  String publisherId = UUID.randomUUID().toString();
 		  if (client == null || (client != null && !client.isConnected())) {
-			  client =  new MqttClient("tcp://localhost:1883",publisherId);
+			  client =  new MqttClient("tcp://localhost:1883",publisherId,new MqttDefaultFilePersistence("/tmpMQTT"));
 			  client.setCallback(new MqttCallback() {
 	            @Override
 	            public void connectionLost(Throwable throwable) {
